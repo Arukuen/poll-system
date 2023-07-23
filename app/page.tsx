2d2799ecbc971422585b337329a2354a42053b9e
Arukuen <adsarmiento3@up.edu.ph>
2023-07-23 21:40:02 +0800

    fix: move components to folder to app folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
-import { PollList } from "@/components/poll/poll-list";
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from './firebase';
 
-export default function Home() {
+import { PollList } from './components/poll/poll-list';
+
+export type User = {
+  name: string,
+  photo: string,
+  id: string,
+}
+
+async function getUsers() {
+  const users: User[] = []
+  const querySnapshot = await getDocs(collection(db, 'users'));
+  querySnapshot.forEach((doc) => {
+    users.push({
+      name: doc.data().name,
+      photo: doc.data().photo,
+      id: doc.id,
+    })
+  });
+  return users;
+}
+
+export default async function Home() {
+  const users = await getUsers();
   return (
-    <main className="flex flex-col items-center p-6">
-      <PollList />
+    <main className='flex flex-col items-center p-6'>
+      <PollList users={users} />
     </main>
   )
 }
--- components/navbar.tsx
+++
@@ -1,8 +0,0 @@
-export default function NavBar() {
-    return (
-      <nav className='flex flex-row justify-between items-center w-ful h-fit py-4 px-6'>
-        <h1 className='text-3xl font-extrabold'>Poll System</h1>
-        <button className='border-solid border-white rounded-xl border-2 p-2'>Create a Poll</button>
-      </nav>
-    )
-}
