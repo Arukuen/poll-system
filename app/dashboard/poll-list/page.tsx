e0cc178ef539fced11c70a10f2d61e81ce6d4125
Arukuen <adsarmiento3@up.edu.ph>
2023-07-28 15:04:08 +0800

    feat: add all polls pages

--- app/dashboard/poll-list/page.tsx
+++ app/dashboard/poll-list/page.tsx
@@ -1,6 +1,51 @@
-export default function Polls() {
-    return (
-        <>
-        </>
-    )
+import { doc, onSnapshot, getDocs, collection, query } from 'firebase/firestore';
+
+import { db } from '@/app/firebase';
+import { PollListItem } from '@/app/components/poll-list';
+import { User } from '@/app/types';
+
+
+async function fetchAllPolls() {
+	const users: User[] = [];
+
+	const userQuery = query(collection(db, 'users'));
+	const userSnapshot = await getDocs(userQuery);
+	userSnapshot.forEach((doc) => {
+		users.push({
+			id: doc.id,
+			name: doc.data().name,
+			photo: doc.data().photo,
+			polls: [],
+		});
+	});
+
+	for (const user of users) {
+		const pollQuery = query(collection(db, 'users', user.id, 'polls'));
+		const pollSnapshot = await getDocs(pollQuery);
+		pollSnapshot.forEach((doc) => {
+			user.polls.push({
+				id: doc.id,
+				title: doc.data().title
+			})
+		});
+	}
+	
+	return users;
+}
+
+export default async function Polls() {
+	const users = await fetchAllPolls();
+
+	return (
+		<div className='flex flex-col w-96'>
+			<h2 className='text-2xl mb-4'>All Polls</h2>
+			<div>
+				{users.map(({ id, name, photo, polls }) => (
+					polls.map(({ id, title }) => (
+						<PollListItem id={id} title={title} username={name} photo ={photo} key={id} />
+					))
+				))}
+			</div>
+		</div>
+	)
 }
