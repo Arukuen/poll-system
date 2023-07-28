9d796210038c1d69969527bdf8ee763b64d4d716
Arukuen <adsarmiento3@up.edu.ph>
2023-07-28 10:45:50 +0800

    feat: make header text link to dashboard

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 export default function NavBar() {
   return (
     <nav className='flex flex-row justify-between items-center w-ful h-fit py-4 px-6'>
-      <h1 className='text-3xl font-extrabold'>Poll System</h1>
+      <Link href='/dashboard'><h1 className='text-3xl font-extrabold'>Poll System</h1></Link>
       <div className='flex flex-row w-60 items-center justify-between'>
         <u><Link href='/dashboard'>Home</Link></u>
         <u><Link href='/dashboard/poll-list'>Polls</Link></u>
