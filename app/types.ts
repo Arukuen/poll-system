eba681c933f5713f89ef3b1b2741b0971e7cfbad
Arukuen <adsarmiento3@up.edu.ph>
2023-07-28 15:02:57 +0800

    feat: add types in separate file

---
+++ app/types.ts
@@ -0,0 +1,20 @@
+export type User = {
+    id: string,
+    name: string,
+    photo: string,
+    polls: Poll[],
+  }
+  
+  export type Poll = {
+    id: string,
+    title: string,
+    choices?: string[],
+    votes?: Vote[],
+  }
+  
+  export type Vote = {
+    user: User,
+    reason: string,
+    vote_index: number,
+  }
+  
